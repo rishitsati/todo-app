34dddb90d1a0bb4c8cf6e97b5c12ef8fc961a94d
Rishit Sati <riahitsati01@gmail.com>
2025-12-09 18:59:38 +0530

    Initialized the project.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import ToDoList from './components/ToDoList';
+import AddItemForm from './components/AddItemForm'; 
+import { Card } from './components/ui/card';
+
+function App(){
+  return <h1>Hello</h1>
+}
+
+export default App;
